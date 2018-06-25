df02ab1d46b206982888be2b4fa6aac01f4cbcf2
Vladimir Pertsov <vovapertsov@gmail.com>
2018-06-25 15:02:38 +0300

    Writing messages in postgres database

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
 
 const TelegramBot = require('node-telegram-bot-api');
 const config = require('config');
+const pg = require('pg');
+const conString = "postgres://postgres:2017Variankalove@localhost:5432/telegrambot";
 
+const client = new pg.Client(conString);
+
+client.connect();
 
 const  TOKEN = config.get("token");
 
@@ -11,5 +16,23 @@ const bot = new TelegramBot(TOKEN, { polling: true});
 
 bot.on('message', msg => {
     const { chat: { id }} = msg;
-    bot.sendMessage(id, 'Pong')
+    bot.sendMessage(id, 'Pong');
+   // console.log(msg);
+
 });
+
+bot.onText(/\/idi (.+)/, (msg, [source, match]) =>
+{
+    const { chat: { id }} = msg;
+    bot.sendMessage(id, match);
+
+    const text = msg.text.substr(4);
+        // [0]["text"];
+
+    console.log(text);
+
+    client.query('INSERT INTO test(text) VALUES ($1)', [text]);
+  //  console.log(msg);
+
+}
+);
